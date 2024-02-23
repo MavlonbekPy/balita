0aea09b13d50522959772d7fff48b4c2dd2a905c
Mavlonbek <153426906+era1iyevv@users.noreply.github.com>
2024-02-23 13:02:41 +0500

    init

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import home_view, blog_view, category_view, contact_view, blog_single, about_view
+
+urlpatterns = [
+    path('', home_view),
+    path('blog/', blog_view),
+    path('category/', category_view),
+    path('contact/', contact_view),
+    path('about/', about_view),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home_view(request):
+    return render(request, 'index.html')
+
+
+def about_view(request):
+    return render(request, 'about.html')
+
+
+def blog_view(request):
+    return render(request, 'blog.html')
+
+
+def category_view(request):
+    return render(request, 'category.html')
+
+
+def contact_view(request):
+    return render(request, 'contact.html')
+
+
+def blog_single(request):
+    return render(request, 'blog-single.html')
