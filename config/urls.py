cab79e4f647c0e19bbb8efbbd6291f25be6b6540
Mavlonbek <153426906+MavlonbekPy@users.noreply.github.com>
2024-06-21 20:07:39 +0500

    Update urls.py
    debug

--- config/urls.py
+++ config/urls.py
@@ -27,8 +27,8 @@ urlpatterns = [
     re_path(
             r'^media/(?P<path>.*)$', serve,
             {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}
-        )
+        ),
 ]
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
