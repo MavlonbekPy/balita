fa01a343101763bdf82e91719011ac0a94baabbf
Mavlonbek <153426906+MavlonbekPy@users.noreply.github.com>
2024-06-21 20:05:46 +0500

    Update urls.py
    re_path

--- config/urls.py
+++ config/urls.py
@@ -18,10 +18,16 @@ from django.conf import settings
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import path, include
+from django.urls import re_path, path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
+    path('', include('blog.urls')),
+    re_path(r'^static/(?P<path>.*)$', serve),
+    re_path(
+            r'^media/(?P<path>.*)$', serve,
+            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}
+        )
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
