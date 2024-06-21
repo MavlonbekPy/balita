bb1f9edd6051d8d71aee69abe292bcfd3e447e2d
Mavlonbek <153426906+MavlonbekPy@users.noreply.github.com>
2024-06-21 20:08:40 +0500

    Update urls.py
    .

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import path, include
 from django.urls import re_path, path, include
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
