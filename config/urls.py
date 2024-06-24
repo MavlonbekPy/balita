e4802b1146ad96de5dfbd411c7d942e66a72e808
Mavlonbek <153426906+MavlonbekPy@users.noreply.github.com>
2024-06-24 20:55:29 +0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
 from django.contrib import admin
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
-from django.urls import path, include
-# from django.urls import re_path, path, include
-# from django.views.static import serve
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,6 +28,9 @@ urlpatterns = [
     #         r'^media/(?P<path>.*)$', serve,
     #         {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}
     #     ),
+    re_path(r'static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    re_path(r'media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
